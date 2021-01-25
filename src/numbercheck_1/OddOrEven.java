b16016ba22fa1d1c5139e590d5fdbd7570c135ee
DESKTOP-JQ6TELU\Laksamana Zaidan <laksamanazaidan@gmail.com>
2021-01-25 15:14:22 +0700

    first commit

---
+++ src/numbercheck_1/OddOrEven.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numbercheck_1;
+
+/**
+ *
+ * @author Laksamana Zaidan
+ */
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void CheckOddOrEven() {
+        int x;
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        if (x % 2 == 0) {
+            System.out.println("The number entered is an even number.");
+        } else {
+            System.out.println("The number entered is an odd number.");
+        }
+    }
+}
